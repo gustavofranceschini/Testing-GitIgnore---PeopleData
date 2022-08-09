4fbf20c955447d062b5f3b1645928bd2c54adf87
Gustavo Franceschini <gustavo.araujo18@fatec.sp.gov.br>
2022-08-09 16:09:39 -0300

    Testing Csharp People Data

---
+++ Testing-Csharp/Entities/Data.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Globalization;
+
+namespace PeopleData.Entities
+{
+    class Data
+    {
+        CultureInfo CI = CultureInfo.InvariantCulture;
+
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public DateTime DayBirth { get; set; }
+        public double BaseSalary { get; set; }
+        public double BaseSalaryIncrease { get; set; }
+
+        public Data()
+        {
+        }
+
+        public Data(string name, int age, DateTime dayBirth, double baseSalary)
+        {
+            Name = name;
+            Age = age;
+            DayBirth = dayBirth;
+            BaseSalary = baseSalary;
+        }
+
+        public void IncreaseSalary(double porcentage)
+        {
+            BaseSalaryIncrease += BaseSalary + (BaseSalary / 100 * porcentage); 
+        }
+
+        public override string ToString()
+        {
+            return "People data:\n"
+                + "Name: "
+                + Name
+                + "\nAge: "
+                + Age
+                + "\nDay Birth: "
+                + DayBirth.ToShortDateString()
+                + "\nSalary before increase: R$ "
+                + BaseSalary.ToString("F2", CI)
+                +"\nSalary after increase: R$ "
+                + BaseSalaryIncrease.ToString("F2", CI);
+                
+            
+                
+                
+                
+        }
+    }
+}
---
+++ Testing-Csharp/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Globalization;
+using PeopleData.Entities;
+
+namespace PeopleData
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CultureInfo CI = CultureInfo.InvariantCulture;
+
+            Console.WriteLine("Enter data people:");
+            Console.Write("Name: ");
+            string name = Console.ReadLine();
+            Console.Write("Age: ");
+            int age = int.Parse(Console.ReadLine());
+            Console.Write("Day Birth: ");
+            DateTime dayBirth = DateTime.Parse(Console.ReadLine());
+            Console.Write("Base Salary: R$ ");
+            double baseSalary = double.Parse(Console.ReadLine(), CI);
+            Console.Write("Percentage increase: ");
+            double percentage = double.Parse(Console.ReadLine(), CI);
+
+            Data people = new Data(name, age, dayBirth, baseSalary);
+            people.IncreaseSalary(percentage);
+
+            Console.WriteLine();
+            Console.WriteLine(people);
+            
+            
+            
+        }
+    }
+}
